839a55e61bf3dbd861efb0a8824b2f623b74f287
yachiy <kenta.yachida@gmail.com>
2024-04-18 22:56:03 +0900

    テスト実行前にローカルのMySQLに接続する設定追加

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import mysql from 'mysql2';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -15,4 +16,20 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test.beforeAll(() => {
+  const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'db'
+  });
+  connection.connect((error) => {
+    if (error) {
+      console.error("Error connecting to MySQL: ", error);
+      return;
+    }
+    console.log("Success connecting to MySQL");
+  });
 });
